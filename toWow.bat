01dcbbd32aa5bddeae2fdc2450f5b5a6b2c61998
ryuusouki <56426191+ryuusouki@users.noreply.github.com>
2022-09-01 23:07:26 +0900

    change wow path

--- toWow.bat
+++ toWow.bat
@@ -1,6 +1,6 @@
 @echo off
-rmdir /S /Q "C:\WowClassic\World of Warcraft\_classic_\Interface\AddOns\ShestakUI"
-rmdir /S /Q "C:\WowClassic\World of Warcraft\_classic_\Interface\AddOns\ShestakUI_Config"
-Xcopy /E /I "D:\WOW UI\SI\github\ShestakUI_Classic\ShestakUI" "C:\WowClassic\World of Warcraft\_classic_\Interface\AddOns\ShestakUI"
-Xcopy /E /I "D:\WOW UI\SI\github\ShestakUI_Classic\ShestakUI_Config" "C:\WowClassic\World of Warcraft\_classic_\Interface\AddOns\ShestakUI_Config"
+rmdir /S /Q "E:\WowClassic\World of Warcraft\_classic_\Interface\AddOns\ShestakUI"
+rmdir /S /Q "E:\WowClassic\World of Warcraft\_classic_\Interface\AddOns\ShestakUI_Config"
+Xcopy /E /I "D:\WOW UI\SI\github\ShestakUI_Classic\ShestakUI" "E:\WowClassic\World of Warcraft\_classic_\Interface\AddOns\ShestakUI"
+Xcopy /E /I "D:\WOW UI\SI\github\ShestakUI_Classic\ShestakUI_Config" "E:\WowClassic\World of Warcraft\_classic_\Interface\AddOns\ShestakUI_Config"
 pause
